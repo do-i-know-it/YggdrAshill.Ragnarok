6691dd546f3c6e0d40bdcd81c7b01ce7fc59a4dc
know <do.i.know.it@yggdrashill.com>
2020-11-27 17:03:28 +0900

    Add implementations for Administraction

---
+++ YggdrAshill.Ragnarok/Administration/Termination.cs
@@ -0,0 +1,42 @@
+﻿using YggdrAshill.Ragnarok.Administration;
+using System;
+
+namespace YggdrAshill.Ragnarok
+{
+    public sealed class Termination :
+        ITermination
+    {
+        private readonly Action onTerminated;
+
+        #region Constructor
+
+        public Termination(Action onTerminated)
+        {
+            if (onTerminated == null)
+            {
+                throw new ArgumentNullException(nameof(onTerminated));
+            }
+
+            this.onTerminated = onTerminated;
+        }
+
+        public Termination()
+        {
+            onTerminated = () =>
+            {
+
+            };
+        }
+
+        #endregion
+
+        #region ITermination
+
+        public void Terminate()
+        {
+            onTerminated.Invoke();
+        }
+
+        #endregion
+    }
+}
