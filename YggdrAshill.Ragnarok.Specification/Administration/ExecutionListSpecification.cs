0c771e797d9c020727a36f294f1ce8353e28b1fe
know <do.i.know.it@yggdrashill.com>
2020-12-11 09:47:33 +0900

    Add implementations for Administration

---
+++ YggdrAshill.Ragnarok.Specification/Administration/ExecutionListSpecification.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace YggdrAshill.Ragnarok.Specification
+{
+    [TestFixture(TestOf = typeof(ExecutionList))]
+    internal class ExecutionListSpecification
+    {
+        private ExecutionList executionList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            executionList = new ExecutionList();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            executionList = default;
+        }
+
+        [Test]
+        public void CollectedShouldExecuteWhenHasExecuted()
+        {
+            var expected = false;
+            var execution = new Execution(() =>
+            {
+                expected = true;
+            });
+
+            executionList.Collect(execution);
+
+            executionList.Execute();
+
+            Assert.IsTrue(expected);
+        }
+
+        [Test]
+        public void CannotCollectNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                executionList.Collect(null);
+            });
+        }
+    }
+}
