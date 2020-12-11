65eb9d28b80be1427ecde1675418efdaa9d909a2
know <do.i.know.it@yggdrashill.com>
2020-12-11 09:39:31 +0900

    Add implementation for Administration

---
+++ YggdrAshill.Ragnarok/Administration/Abortion.cs
@@ -0,0 +1,47 @@
+﻿using YggdrAshill.Ragnarok.Administration;
+using System;
+
+namespace YggdrAshill.Ragnarok
+{
+    public sealed class Abortion :
+        IAbortion
+    {
+        private readonly Action<Exception> onAborted;
+
+        #region Constructor
+
+        public Abortion(Action<Exception> onAborted)
+        {
+            if (onAborted == null)
+            {
+                throw new ArgumentNullException(nameof(onAborted));
+            }
+
+            this.onAborted = onAborted;
+        }
+
+        public Abortion()
+        {
+            onAborted = (_) =>
+            {
+
+            };
+        }
+
+        #endregion
+
+        #region IAbortion
+
+        public void Abort(Exception exception)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            onAborted.Invoke(exception);
+        }
+
+        #endregion
+    }
+}
