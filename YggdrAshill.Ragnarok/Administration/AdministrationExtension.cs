14be0c6598056d3bfa1274a415f6ace2e254eabe
know <do.i.know.it@yggdrashill.com>
2020-12-11 09:47:47 +0900

    Add extension for Administration

---
+++ YggdrAshill.Ragnarok/Administration/AdministrationExtension.cs
@@ -0,0 +1,36 @@
+﻿using YggdrAshill.Ragnarok.Administration;
+using System;
+
+namespace YggdrAshill.Ragnarok
+{
+    public static class AdministrationExtension
+    {
+        public static void Collect(this ITermination termination, ITerminationCollection collection)
+        {
+            if (termination == null)
+            {
+                throw new ArgumentNullException(nameof(termination));
+            }
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            collection.Collect(termination);
+        }
+
+        public static void Collect(this IExecution execution, IExecutionCollection collection)
+        {
+            if (execution == null)
+            {
+                throw new ArgumentNullException(nameof(execution));
+            }
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            collection.Collect(execution);
+        }
+    }
+}
