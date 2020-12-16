20cde3d278f1e7b9ff0e2399c9d39d120a9382b9
know <do.i.know.it@yggdrashill.com>
2020-12-16 23:53:13 +0900

    Add extension for Unification

--- YggdrAshill.Ragnarok/Unification/UnificationExtension.cs
+++ YggdrAshill.Ragnarok/Unification/UnificationExtension.cs
@@ -7,6 +7,20 @@ namespace YggdrAshill.Ragnarok
 {
     public static class UnificationExtension
     {
+        public static void Collect(this ITerminationCollection collection, Action onTerminated)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            if (onTerminated == null)
+            {
+                throw new ArgumentNullException(nameof(onTerminated));
+            }
+
+            collection.Collect(new Termination(onTerminated));
+        }
+
         public static void Collect(this ITermination termination, ITerminationCollection collection)
         {
             if (termination == null)
@@ -21,6 +35,20 @@ namespace YggdrAshill.Ragnarok
             collection.Collect(termination);
         }
 
+        public static ITermination Collect(this IExecutionCollection collection, Action onExecuted)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+            if (onExecuted == null)
+            {
+                throw new ArgumentNullException(nameof(onExecuted));
+            }
+
+            return collection.Collect(new Execution(onExecuted));
+        }
+
         public static ITermination Collect(this IExecution execution, IExecutionCollection collection)
         {
             if (execution == null)
@@ -33,6 +61,23 @@ namespace YggdrAshill.Ragnarok
             }
 
             return collection.Collect(execution);
+        }
+
+        public static IOrigination Collect(this IActivation activation, IExecutionCollection collection)
+        {
+            if (activation == null)
+            {
+                throw new ArgumentNullException(nameof(activation));
+            }
+            if (collection == null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            return new Origination(() =>
+            {
+                return activation.Activate().Collect(collection);
+            });
         }
     }
 }
