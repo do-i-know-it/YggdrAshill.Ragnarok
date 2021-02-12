9e156ddff96a9a138dcbf976dc21625ee484bf03
know <do.i.know.it@yggdrashill.com>
2021-02-12 13:46:54 +0900

    Add implementation for Progression

---
+++ YggdrAshill.Ragnarok/Progression/CompositeExecution.cs
@@ -0,0 +1,48 @@
+﻿using YggdrAshill.Ragnarok.Periodization;
+using YggdrAshill.Ragnarok.Progression;
+using System;
+using System.Collections.Generic;
+
+namespace YggdrAshill.Ragnarok
+{
+    public sealed class CompositeExecution :
+        IExecution,
+        ITermination
+    {
+        private readonly List<IExecution> executionList = new List<IExecution>();
+
+        public ITermination Bind(IExecution execution)
+        {
+            if (execution == null)
+            {
+                throw new ArgumentNullException(nameof(execution));
+            }
+
+            if (!executionList.Contains(execution))
+            {
+                executionList.Add(execution);
+            }
+
+            return new Termination(() =>
+            {
+                if (executionList.Contains(execution))
+                {
+                    executionList.Remove(execution);
+                }
+            });
+        }
+
+        public void Execute()
+        {
+            foreach (var execution in executionList)
+            {
+                execution.Execute();
+            }
+        }
+
+        public void Terminate()
+        {
+            executionList.Clear();
+        }
+    }
+}
