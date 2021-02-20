2380d7a4cfa8d9ccd7bda3a7c6d352665cdcec09
know <do.i.know.it@yggdrashill.com>
2021-02-20 23:36:45 +0900

    Add document comments for Periodization

--- Periodization/ITermination.cs
+++ Periodization/ITermination.cs
@@ -1,7 +1,13 @@
 ﻿namespace YggdrAshill.Ragnarok.Periodization
 {
+    /// <summary>
+    /// Token to stop, end or finalize.
+    /// </summary>
     public interface ITermination
     {
+        /// <summary>
+        /// Terminates. 
+        /// </summary>
         void Terminate();
     }
 }
--- YggdrAshill.Ragnarok/Periodization/CompositeTermination.cs
+++ YggdrAshill.Ragnarok/Periodization/CompositeTermination.cs
@@ -4,11 +4,24 @@ using System.Collections.Generic;
 
 namespace YggdrAshill.Ragnarok
 {
+    /// <summary>
+    /// Implementation of <see cref="ITermination"/>.
+    /// Collects other tokens of <see cref="ITermination"/> to execute when this has terminated.
+    /// </summary>
     public sealed class CompositeTermination :
         ITermination
     {
         private readonly List<ITermination> terminationList = new List<ITermination>();
 
+        /// <summary>
+        /// Binds <see cref="ITermination"/>.
+        /// </summary>
+        /// <param name="termination">
+        /// <see cref="ITermination"/> to bind.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="termination"/> is null.
+        /// </exception>
         public void Bind(ITermination termination)
         {
             if (termination == null)
@@ -24,6 +37,9 @@ namespace YggdrAshill.Ragnarok
             terminationList.Add(termination);
         }
 
+        /// <summary>
+        /// Executes each <see cref="ITermination"/> when this has terminated
+        /// </summary>
         public void Terminate()
         {
             foreach (var termination in terminationList)
