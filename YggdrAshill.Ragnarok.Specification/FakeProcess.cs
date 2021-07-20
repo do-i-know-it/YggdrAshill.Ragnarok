ce8453eb82687e42f831f56b555d66635ef4e1f2
know <do.i.know.it@yggdrashill.com>
2021-07-20 11:35:40 +0900

    Modify specifications for Progression

---
+++ YggdrAshill.Ragnarok.Specification/FakeProcess.cs
@@ -0,0 +1,33 @@
+﻿using YggdrAshill.Ragnarok.Progression;
+
+namespace YggdrAshill.Ragnarok.Specification
+{
+    internal class FakeProcess :
+        IProcess
+    {
+        private readonly FakePeriod period = new FakePeriod();
+
+        private readonly FakeExecution execution = new FakeExecution();
+
+        internal bool Originated => period.Originated;
+
+        internal bool Terminated => period.Terminated;
+
+        internal bool Executed => execution.Executed;
+
+        public void Originate()
+        {
+            period.Originate();
+        }
+
+        public void Terminate()
+        {
+            period.Terminate();
+        }
+
+        public void Execute()
+        {
+            execution.Execute();
+        }
+    }
+}
