327948aaeee054b7371c66e44706fd616808f899
know <do.i.know.it@yggdrashill.com>
2023-05-18 00:01:02 +0900

    Modify method of interface as obsolute

--- Fabrication/IAssignAnyType.cs
+++ Fabrication/IAssignAnyType.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace YggdrAshill.Ragnarok.Fabrication
 {
     public interface IAssignAnyType
     {
         IAfterAnyTypeAssigned As<T>() where T : notnull;
+        [Obsolete]
         IAfterImplementedTypeAssigned AsSelf();
         IAfterImplementedInterfacesAssigned AsImplementedInterfaces();
     }
