e62099b61491841b07baff48efb8b8df862cb52e
know <do.i.know.it@yggdrashill.com>
2024-02-08 18:19:45 +0900

    Remove unnecessary implementations

--- YggdrAshill.Ragnarok/Extension/ObjectResolverExtension.cs
+++ YggdrAshill.Ragnarok/Extension/ObjectResolverExtension.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace YggdrAshill.Ragnarok
 {
     // TODO: add document comments.
@@ -8,19 +6,6 @@ namespace YggdrAshill.Ragnarok
         public static T Resolve<T>(this IObjectResolver resolver)
         {
             return (T)resolver.Resolve(typeof(T));
-        }
-
-        public static object Resolve(this IObjectResolver resolver, IEnumerable<IParameter> parameterList, Argument argument)
-        {
-            foreach (var parameter in parameterList)
-            {
-                if (parameter.CanResolve(argument, out var instance))
-                {
-                    return instance;
-                }
-            }
-
-            return resolver.Resolve(argument.Type);
         }
     }
 }
--- YggdrAshill.Ragnarok/Implementation/Installation.cs
+++ YggdrAshill.Ragnarok/Implementation/Installation.cs
@@ -1,22 +1,10 @@
 using System;
-using System.Collections.Generic;
 
 namespace YggdrAshill.Ragnarok
 {
     internal sealed class Installation : IInstallation
     {
         private readonly Action<IObjectContainer> onInstalled;
-
-        public Installation(IEnumerable<IInstallation> installationList)
-        {
-            onInstalled = container =>
-            {
-                foreach (var installation in installationList)
-                {
-                    installation.Install(container);
-                }
-            };
-        }
 
         public Installation(Action<IObjectContainer> onInstalled)
         {
