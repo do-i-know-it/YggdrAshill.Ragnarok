5cb921270bbf31df9fa84cf0c5ffc803c0669824
know <do.i.know.it@yggdrashill.com>
2024-02-16 18:44:27 +0900

    Modify specifications for framework

---
+++ YggdrAshill.Ragnarok.Specification/CreateObject.cs
@@ -0,0 +1,17 @@
+﻿namespace YggdrAshill.Ragnarok.Specification
+{
+    internal sealed class CreateObject : ICreation<object>
+    {
+        public static CreateObject Instance { get; } = new CreateObject();
+
+        private CreateObject()
+        {
+
+        }
+
+        public object Create()
+        {
+            return new object();
+        }
+    }
+}
